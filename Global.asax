5689ac626030396bc0e93eaa7d367bfa0ac88d78
Muhammad M. Fadel <33105817+MuhammadFadel@users.noreply.github.com>
2018-06-20 05:07:40 +0200

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineBusReservationV6.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineBusReservationV6.Startup))]
+namespace OnlineBusReservationV6
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
